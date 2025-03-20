8d73e71eeee8c7aab9764fc662eb318228ab7b9f
Gabriel B. <53912956+lookin-at-the-stars@users.noreply.github.com>
2025-03-20 19:59:46 -0300

    Update Reserva.java

--- CrispyGarbanzo/src/Reserva.java
+++ CrispyGarbanzo/src/Reserva.java
@@ -8,19 +8,6 @@ class Reserva {
     private LocalDate checkOut;
     private double custoTotal;
 
-    public Reserva(Propriedade propriedade, Locatario locatario, LocalDate checkIn, LocalDate checkOut) {
-        this.propriedade = propriedade;
-        this.locatario = locatario;
-        this.checkIn = checkIn;
-        this.checkOut = checkOut;
-        this.custoTotal = calcularCustoTotal();
-    }
-
-    public double calcularCustoTotal() {
-        long noites = ChronoUnit.DAYS.between(checkIn, checkOut);
-        return noites * propriedade.getPrecoPorNoite();
-    }
-
     public void imprimirReserva() {
         System.out.println("=== Detalhes da Reserva ===");
         System.out.println("Propriedade: " + propriedade.getTitulo());
@@ -29,20 +16,7 @@ class Reserva {
         System.out.println("Check-out: " + checkOut);
         System.out.println("Custo Total: R$" + custoTotal);
     }
-
-    public static Reserva fazerReserva(Propriedade propriedade, Locatario locatario, LocalDate checkIn, LocalDate checkOut) {
-        if (!propriedade.verificarDisponibilidade()) {
-            System.out.println("A propriedade não está disponível para reserva.");
-            return null;
-        }
-
-        Reserva reserva = new Reserva(propriedade, locatario, checkIn, checkOut);
-        propriedade.setDisponibilidade(false);
-        System.out.println("Reserva realizada com sucesso!");
-        return reserva;
-    }
-
-    // Getters e Setters
+    
     public Propriedade getPropriedade() {
         return propriedade;
     }
